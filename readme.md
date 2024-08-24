4743b95c6276061cc5bda092e53cb90cb5ab8055
looom <looom@kali>
2024-08-24 12:31:08 +0800

    wkyd_dbshow

---
+++ readme.md
@@ -0,0 +1,5 @@
+# wkyd_dbshow
+地图筛选器  
+配合[wkyd Debugger nodejs](https://github.com/xmexg/wdn)使用
+
+![image](./img/img1.png)
---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+    <div id="page">
+        <h4>适用于wsb/n的地图筛选器</h4>
+        <MapsList />
+    </div>
+</template>
+
+<script setup>
+import MapsList from './MapsList.vue';
+</script>
+
+<style scoped>
+    #page {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        height: 100%;
+        width: 100%;
+        padding: 0;
+        margin: 0;
+    }
+</style>
